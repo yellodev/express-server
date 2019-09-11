a1022943e5bc58eec0e4337691a410d2670094d5
Reef <dev@yello.re>
2019-09-11 10:40:51 +0400

    first commit

--- express/server.js
+++ express/server.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
+  console.log('Web App')
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
